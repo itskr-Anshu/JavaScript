93e3f6a216ed9e985ff197b55a405dcfc9ec4528
itskr-Anshu <167881439+itskr-Anshu@users.noreply.github.com>
2025-07-13 14:01:13 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+console.log("Data Types")
+
+let name = "Anshu" //string
+let age = 19       //intger
+let canVote = true;//boolean
+
+// null -> standalone value
+// undefined -> 
+// symbol -> unique k liye
+// object -> 
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined

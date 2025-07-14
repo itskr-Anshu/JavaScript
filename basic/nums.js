cdc556b38e21e214d775ebbdbb263b099b3ce674
itskr-Anshu <167881439+itskr-Anshu@users.noreply.github.com>
2025-07-14 15:17:25 +0000

    nums

---
+++ basic/nums.js
@@ -0,0 +1,9 @@
+const a = new Number(100)
+console.log(a)
+console.log(a.toString().length)
+console.log(a.toFixed(2)) // 100.00
+console.log(Math.random())
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)

5f53e8195a7d8f59e25771cb2cccd995a831e806
itskr-Anshu <167881439+itskr-Anshu@users.noreply.github.com>
2025-07-14 14:52:04 +0000

    string

---
+++ basic/string.js
@@ -0,0 +1,14 @@
+const name =  "Anshuu"
+const id  = 12
+
+console.log(`My name is ${name} and my id is ${id}`)
+console.log(name.charAt(3)) // h
+console.log(name.indexOf('u')) // 4
+const s = "  Anshu   kumar"
+console.log(s.trim()) //Anshu    Kumar
+
+// replace -> name.replace("old" , "new")
+
+//split
+
+//

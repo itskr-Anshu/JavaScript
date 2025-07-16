9706716777d3b416500de6efbdc6f20b88b077b5
itskr-Anshu <167881439+itskr-Anshu@users.noreply.github.com>
2025-07-16 19:13:19 +0000

    arrowfun

---
+++ Function/arrowfun.js
@@ -0,0 +1,12 @@
+const two = (a , b) =>{
+    return (a+b)
+}
+//   ...or....
+const add = (a , b) => a*b
+//   ...or....
+const sub = (a , b) => (a-b)
+
+
+console.log(two(5,5))
+console.log(add(5,5))
+console.log(sub(5,5))
